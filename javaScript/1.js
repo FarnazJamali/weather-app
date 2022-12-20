538520431e4c9d63c533383571089bf097404c57
FarnazJamali <fz.jamali@gmail.com>
2022-12-20 10:38:31 +0330

    [MODIFIED] working on API

--- javaScript/1.js
+++ javaScript/1.js
@@ -10,16 +10,43 @@ function changeData(res) {
   console.log(res.data);
 }
 
-console.log(city);
-let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=${apiKey}&units=metric`;
-
-axios.get(url).then(changeData);
 
 function changeCity(event) {
   event.preventDefault();
   let input = document.getElementById("input_value");
   document.getElementById("city").innerHTML = input.value;
 }
-let form = document.getElementById("form");
-form.addEventListener("submit", changeCity);
+document.getElementById("form").addEventListener("submit", changeCity);
+
+let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=${apiKey}&units=metric`;
+
+axios.get(url).then(changeData);
+
+function showLocation(location) {
+  console.log(location);
+  let lat = location.coords.latitude;
+  let lon = location.coords.longitude;
+  console.log(lat, lon);
+
+  function showCurrent(loc) {
+    document.getElementById("city").innerHTML = loc.data.name;
+    document.querySelector("h1").innerHTML = Math.round(loc.data.main.temp);
+    document.querySelector("#description").innerHTML = loc.data.weather[0].main;
+    document.querySelector("#humidity").innerHTML =
+      "Humidity: " + loc.data.main.humidity + "%";
+    document.querySelector("#wind").innerHTML =
+      "Wind speed: " + loc.data.wind.speed + " km/h";
+    console.log(loc.data);
+  }
+  let urlSecond = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(urlSecond).then(showCurrent);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showLocation);
+}
+
+document
+  .getElementById("current")
+  .addEventListener("click", getCurrentPosition);
