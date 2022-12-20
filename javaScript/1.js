21a3a2be3c3a5ceb2a193d39a5eae0fb37554312
FarnazJamali <fz.jamali@gmail.com>
2022-12-20 12:19:08 +0330

    [MODIFIED] finished working on API

--- javaScript/1.js
+++ javaScript/1.js
@@ -1,6 +1,23 @@
-let city = document.getElementById("city").innerHTML;
+function formatDate() {
+  let week = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+  let now = new Date();
+  let day = week[now.getDay() - 1];
+  let time = now.getHours() + ":" + now.getMinutes();
+  return time;
+}
+let newDate = new Date();
+document.querySelector("#date").innerHTML = formatDate(newDate);
+
+
 function changeData(res) {
-  console.log(res.data.weather[0].main);
   document.querySelector("h1").innerHTML = Math.round(res.data.main.temp);
   document.querySelector("#description").innerHTML = res.data.weather[0].main;
   document.querySelector("#humidity").innerHTML =
@@ -10,18 +27,21 @@ function changeData(res) {
   console.log(res.data);
 }
 
+function search(city) {
+   let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   axios.get(url).then(changeData);
+}
 
 function changeCity(event) {
   event.preventDefault();
   let input = document.getElementById("input_value");
+  let city = input.value;
   document.getElementById("city").innerHTML = input.value;
+  search(city);
 }
+search("Tehran");
 document.getElementById("form").addEventListener("submit", changeCity);
-
-let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=${apiKey}&units=metric`;
-
-axios.get(url).then(changeData);
 
 function showLocation(location) {
   console.log(location);
@@ -39,6 +59,7 @@ function showLocation(location) {
       "Wind speed: " + loc.data.wind.speed + " km/h";
     console.log(loc.data);
   }
+  let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
   let urlSecond = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(urlSecond).then(showCurrent);
 }
